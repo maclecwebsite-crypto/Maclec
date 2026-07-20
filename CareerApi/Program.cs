5e5a0a9a771f7aa43e92e82f9671e061718574d0
Manav Jain <manav7421@gmail.com>
2026-07-21 00:00:30 +0530

    re changes

--- CareerApi/Program.cs
+++ CareerApi/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<CareerDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("CareerDb"),
-        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("CareerDb"))
+        new MySqlServerVersion(new Version(8, 0, 36))
     ));
 
 const string CareerSitePolicy = "CareerSitePolicy";
@@ -25,13 +25,14 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-app.UseHttpsRedirection();
-app.UseCors(CareerSitePolicy);
-app.UseAuthorization();
-app.MapControllers();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<CareerDbContext>();
     db.Database.Migrate();
 }
+
+app.UseHttpsRedirection();
+app.UseCors(CareerSitePolicy);
+app.UseAuthorization();
+app.MapControllers();
 app.Run();
