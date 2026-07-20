a4542ae4e39519700d468df57be703905ac77090
Manav Jain <manav7421@gmail.com>
2026-07-20 22:29:48 +0530

    migration

--- CareerApi/Program.cs
+++ CareerApi/Program.cs
@@ -30,7 +30,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<CareerDbContext>();
-    db.Database.Migrate();
+    db.Database.EnsureCreated();
 }
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
